2366cdac1e02aa333cd53f48c1440e67d5a22ac1
gohpeijin <gohpeijin@gmail.com>
2021-07-06 04:10:28 +0800

    adding files

---
+++ README.md
@@ -0,0 +1,3 @@
+# Simple Markdown Editor
+
+The preview project available at [here](https://gohpeijin.github.io/markdown-editor).
---
+++ src/App.tsx
@@ -0,0 +1,37 @@
+import React, {useState} from 'react';
+import parse from 'html-react-parser'; //import this to avoid using dangerouslySetInnerHtml 
+import DOMPurify from 'dompurify';
+import './App.css';
+
+function App() {
+
+  const [markdown,setMarkdown] = useState ("***Italic***\n**Bold**\n_Underline_");
+
+
+  // const convertedMarkdown = DOMPurify.sanitize(markdown.replace(/\*\*\*([^]+?)\*\*\*/gm, '<i>$1</i>').replace(/\*\*([^]+?)\*\*/gm, '<b>$1</b>').replace(/_([^]+?)_/gm, '<u>$1</u>').replace(/\n/g, "<br>"), {
+  //   USE_PROFILES: { html: true },
+  // }); // line break will not reset
+
+  const convertedMarkdown = DOMPurify.sanitize(markdown.replace(/\*\*\*(.*?)\*\*\*/gm, '<i>$1</i>').replace(/\*\*(.*?)\*\*/gm, '<b>$1</b>').replace(/_(.*?)_/gm, '<u>$1</u>').replace(/\n/g, "<br>"), {
+    USE_PROFILES: { html: true },
+  }); // line break will reset 
+
+  return (
+   <div>
+    <h1 className="App">Markdown Editor</h1> 
+    <div className="Text">
+      <p>Editor</p><p>Preview</p>
+    </div>
+    <div className="Split">
+    {/* <textarea  value = {markdown} onChange={handleChange} > </textarea> */}
+    <textarea  value = {markdown} onChange={e => setMarkdown(e.target.value)} > </textarea>
+      {/* <textarea   onBlur={handleBlur} > </textarea> */}
+      <div className="Preview">
+         <p>{parse(convertedMarkdown)}</p>
+      </div>
+    </div>
+   </div>
+  );
+}
+
+export default App;
